184adffed010cd6a72e2b7c8fbd7dfa056d18967
dgutierres26 <diegogutierres@hotmail.com>
2021-03-02 11:16:48 +0000

    index

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const http = require('http');
-const axios = require('axios');
+const http = require('http'),
+axios = require('axios'),
+logger = require('morgan'),
+cors = require('cors'),
+express = require('express'),
+bodyParser = require('body-parse');
 
-http.createServer((req, res)=>{
-  res.write(users.join(", ")); //display the list of users on the page
-  res.write("\n\n"+emails.join(", ")); //display the list of users on the page
-  res.end(); //end the response
-}).listen(8000); // listen for requests on port 8000
+var app = express();
+var port = 8000;
+
+app.get('/hello', (req, res) => {
+
+    res.send("Hello BScBest!");
+    
+});
+
+
+// http.createServer((req, res)=>{
+//   res.write(users.join(", ")); //display the list of users on the page
+//   res.write("\n\n"+emails.join(", ")); //display the list of users on the page
+//   res.end(); //end the response
+// }).listen(8000); // listen for requests on port 8000
 
 let users = []; // names of users will be stored here
 let email = [];
@@ -13,11 +27,16 @@ let email = [];
   try{
     const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
     users = data.map(user=>user.name);
-    emails = data.map(email=>email.email);
+//    emails = data.map(email=>email.email);
     console.log(users);
-    console.log(emails);
+//    console.log(emails);
   } catch(error){
     console.log(error)
   }
 })()
+
+app.listen(port, function(err){
+    console.log('Listening on port: ' + port);
+});
+
 
