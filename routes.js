a5ca161c1844d184bbf613255ae09e74ac8073a3
dgutierres26 <diegogutierres@hotmail.com>
2021-03-23 10:05:56 +0000

    intem-controller

--- routes.js
+++ routes.js
@@ -10,14 +10,14 @@ router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
 router.post('/hello', itemCtrl.postWorld);
 
 // router.get('/hello/:foo/:bar', (req, res) => {
-// res.json({message: 'Hello BScBest!', data: [
+//   res.json({message: 'Hello BScBest!', data: [
 //     req.params.foo,
 //     req.params.bar
 // ]});
 // });
 
 // router.post('/hello', (req, res) => {
-// res.json({result: 'Post was sent', data: req.body});
+//  res.json({result: 'Post was sent', data: req.body});
 // });
 
 module.exports = router;
